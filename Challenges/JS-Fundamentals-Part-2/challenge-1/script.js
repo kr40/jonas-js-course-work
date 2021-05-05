43d2024fcd56c9ef8a70fc7919ba981f49757049
Kartik Rao <kr40.git@gmail.com>
2021-05-05 15:47:06 +0000

    Did 3 challenges for JSF-P2

--- Challenges/JS-Fundamentals-Part-2/challenge-1/script.js
+++ Challenges/JS-Fundamentals-Part-2/challenge-1/script.js
@@ -1,1 +1,27 @@
-console.log('Hi form the console');
+'use strict';
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// Test 1
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+	if (avgDolphins >= 2 * avgKoalas) {
+		console.log(`Dolphins win!! (${avgDolphins} vs ${avgKoalas})`);
+	} else if (avgKoalas >= 2 * avgDolphins) {
+		console.log(`Koalas win!! (${avgKoalas} vs ${avgDolphins})`);
+	} else {
+		console.log(`Nobody wins!!`);
+	}
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+// Test 2
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+
+checkWinner(scoreDolphins, scoreKoalas);
--- Challenges/JS-Fundamentals-Part-2/challenge-3/script.js
+++ Challenges/JS-Fundamentals-Part-2/challenge-3/script.js
@@ -1,1 +1,38 @@
-console.log('Hi form the console');
+'use strict';
+
+const Mark = {
+	fullName: 'Mark Miller',
+	mass: 78,
+	height: 1.69,
+	calcBMI: function () {
+		this.bmi = this.mass / this.height ** 2;
+		return this.bmi;
+	},
+};
+
+const John = {
+	fullName: 'John Smith',
+	mass: 92,
+	height: 1.95,
+	calcBMI: function () {
+		this.bmi = this.mass / this.height ** 2;
+		return this.bmi;
+	},
+};
+
+Mark.calcBMI();
+John.calcBMI();
+
+if (Mark.bmi > John.bmi) {
+	console.log(
+		`${Mark.fullName}'s BMI (${Math.floor(Mark.bmi)}) is higher than ${
+			John.fullName
+		}'s BMI (${Math.floor(John.bmi)}).`
+	);
+} else {
+	console.log(
+		`${John.fullName}'s BMI (${Math.floor(John.bmi)}) is higher than ${
+			Mark.fullName
+		}'s BMI (${Math.floor(Mark.bmi)}).`
+	);
+}
