ee9ef9ef2bd8996c01fbd35a211fcae2bf301733
kr40 <kr40.git@gmail.com>
2021-05-09 13:44:33 +0530

    Done with coding challenge 4

--- Challenges/JS-Fundamentals-Part-2/challenge-4/script.js
+++ Challenges/JS-Fundamentals-Part-2/challenge-4/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('Hi form the console');
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+
+for (let i = 0; i < bills.length; i++) {
+	const tip = calcTip(bills[i]);
+	tips.push(tip);
+	totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
