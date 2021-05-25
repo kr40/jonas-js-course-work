7f727cfa1b2b5fb202bbf8225992c465dace697c
kr40 <kr40.git@gmail.com>
2021-05-25 11:03:08 +0530

    done with challenge 2 of DS, Strings

--- Challenges/DS- Modern Operators-Strings/Challenge-2/script.js
+++ Challenges/DS- Modern Operators-Strings/Challenge-2/script.js
@@ -1,3 +1,59 @@
 'use strict';
 
-console.log('Hi form the console');
+const game = {
+	team1: 'Bayern Munich',
+	team2: 'Borrussia Dortmund',
+	players: [
+		[
+			'Neuer',
+			'Pavard',
+			'Martinez',
+			'Alaba',
+			'Davies',
+			'Kimmich',
+			'Goretzka',
+			'Coman',
+			'Muller',
+			'Gnarby',
+			'Lewandowski',
+		],
+		['Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Witsel', 'Hazard', 'Brandt', 'Sancho', 'Gotze'],
+	],
+	score: '4:0',
+	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+	date: 'Nov 9th, 2037',
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
+};
+
+//* 1: Loop over game.scored
+
+for (const [i, player] of game.scored.entries()) console.log(`Goal ${i + 1} : ${player}`);
+
+//* 2: Calculate the average odd
+
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+//* 3: Print the 3 odds
+
+for (const [team, odd] of Object.entries(game.odds)) {
+	const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+	console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+//* 4: BONUS
+
+const scorers = {};
+for (const player of game.scored) {
+	scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+console.log(scorers);
