a687b8782a3b4844211c6ad46bfe16be7afb2726
kr40 <kr40.git@gmail.com>
2021-06-14 16:12:38 +0530

    done with challenge 4

--- Challenges/DS- Modern Operators-Strings/Challenge-4/script.js
+++ Challenges/DS- Modern Operators-Strings/Challenge-4/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hi form the console');
+document.querySelector('button').addEventListener('click', function () {
+	const text = document.querySelector('textarea').value;
+	const rows = text.split('\n');
+
+	for (const [i, row] of rows.entries()) {
+		const [first, second] = row.toLowerCase().trim().split('_');
+		const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+		console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+	}
+});
