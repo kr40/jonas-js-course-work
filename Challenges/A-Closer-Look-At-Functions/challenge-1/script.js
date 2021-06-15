f590a9500a31ce0da26f992e32d93c08a6833738
kr40 <kr40.git@gmail.com>
2021-06-15 13:37:59 +0530

    done with challenge

--- Challenges/A-Closer-Look-At-Functions/challenge-1/script.js
+++ Challenges/A-Closer-Look-At-Functions/challenge-1/script.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('Hi form the console');
+const poll = {
+	question: 'What is your favourite programming language?',
+	options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+	// This generates [0, 0, 0, 0]. More in the next section!
+	answers: new Array(4).fill(0),
+
+	//* Get user input
+	registerNewAnswer() {
+		const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+
+		//* Register the answer
+		typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+		this.displayResults();
+		this.displayResults('string');
+	},
+
+	//* Display the calculated poll
+	displayResults(type = 'array') {
+		if (type === 'array') {
+			console.log(this.answers);
+		} else if (type === 'string') {
+			console.log(`Poll results are ${this.answers.join(', ')}`);
+		}
+	},
+};
+
+document.querySelector('button').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6] });
