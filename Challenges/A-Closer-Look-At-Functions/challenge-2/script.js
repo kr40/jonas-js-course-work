c8a50810dd93445b45647fe250d727747a265316
kr40 <kr40.git@gmail.com>
2021-06-15 17:29:17 +0530

    done with challenge

--- Challenges/A-Closer-Look-At-Functions/challenge-2/script.js
+++ Challenges/A-Closer-Look-At-Functions/challenge-2/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hi form the console');
+(function () {
+	const header = document.querySelector('h1');
+	header.style.color = 'red';
+
+	document.querySelector('body').addEventListener('click', function () {
+		header.style.color = 'blue';
+	});
+})();
+
+// IIFE executed and exited the EC, but, the next function remembers the variables of its birthplace and hence it works.
