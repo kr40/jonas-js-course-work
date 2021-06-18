eb0f10954d5722fb6d0cec591e8ca8f0d4378b08
kr40 <kr40.git@gmail.com>
2021-06-18 14:21:14 +0530

    done with challenge 1

---
+++ Challenges/Working-with-arrays/challenge-1/script.js
@@ -0,0 +1,19 @@
+'use strict';
+const dogsJulia = [];
+const dogsKate = [];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+	const dogsJuliaCopy = dogsJulia.slice(1, 3);
+
+	const correctedDogs = [...dogsJuliaCopy, ...dogsKate];
+
+	correctedDogs.forEach(function dogAge(age, i) {
+		const dogAge = age >= 3 ? 'Adult' : 'Puppy';
+		console.log(`Dog number ${i + 1} is a ${dogAge} and is ${age} years old.`);
+	});
+};
+
+console.log('Test Data 1');
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log('Test Data 2');
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
--- Projects/Project-4-Bankist App/script.js
+++ Projects/Project-4-Bankist App/script.js
@@ -61,6 +61,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+	containerMovements.innerHTML = '';
+
+	movements.forEach(function (mov, i) {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+		const html = `		<div class="movements__row">
+					<div class="movements__type movements__type--${type}">${i + 1} ${type}</div><div class="movements__value">${mov}</div>`;
+
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
+};
+
+displayMovements(account1.movements);
+
 // ///////////////////////////////////////////////
 // ///////////////////////////////////////////////
 // LECTURES
@@ -74,3 +89,21 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // ///////////////////////////////////////////////
+
+// for (const [i, mov] of movements.entries()) {
+// 	if (mov > 0) {
+// 		console.log(`Movement ${i + 1}: You deposited ${mov}`);
+// 	} else {
+// 		console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+// 	}
+// }
+
+// console.log('----forEach----');
+
+// movements.forEach(function (mov, i, arr) {
+// 	if (mov > 0) {
+// 		console.log(`Movement ${i + 1}: You deposited ${mov}`);
+// 	} else {
+// 		console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+// 	}
+// });
